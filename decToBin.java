029ef30b233d0297ad9ab07bdf8fbc7af132a80f
Shrawan2002 <shrawankumar1754@gmail.com>
2024-09-10 17:12:01 +0530

    initial files

---
+++ decToBin.java
@@ -0,0 +1,18 @@
+public class decToBin {
+
+    public static void decToBinary(int decNum){
+        int myNum = decNum;
+        int pow = 0;
+        int binNum = 0;
+        while(decNum>0){
+            int rem = decNum % 2;
+            binNum = binNum + rem * (int)Math.pow(10,pow);
+            decNum = decNum/2;
+            pow++;
+        }
+        System.out.println("binary of " + decNum + " = " + binNum);
+    }
+    public static void main(String[] args) {
+        decToBinary(7);
+    }
+}
