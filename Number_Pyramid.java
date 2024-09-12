fde47a68f4a872854449a6280f1f2b4abacf8d68
Shrawan2002 <shrawankumar1754@gmail.com>
2024-09-12 22:20:04 +0530

    initial files

--- Number_Pyramid.java
+++ Number_Pyramid.java
@@ -1,15 +1,15 @@
 public class Number_Pyramid {
     public static void number_Pyramid(int n){
-        int count = 1;
+        // int count = 1;
         for(int i = 1; i<=n; i++){
             //space 
             for(int j=1; j<=n-i; j++){
                 System.out.print(" ");
             }
             for(int j=1; j<=i; j++){
-                System.out.print(count + " ");
+                System.out.print(i + " ");
             }
-            count++;
+            // count++;
             System.out.println("");
         }
     }
