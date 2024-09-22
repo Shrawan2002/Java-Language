95edefec9f5477a8690f38b6277b367c9c3ea99c
Shrawan2002 <shrawankumar1754@gmail.com>
2024-09-22 21:33:40 +0530

    initial files

--- keySearch2D_array.java
+++ keySearch2D_array.java
@@ -18,17 +18,21 @@ public class keySearch2D_array {
         int matrix[][] = new int[3][3];
         int n = matrix.length, m = matrix[0].length;
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 9 elements for the 3x3 matrix:");
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 matrix[i][j] = sc.nextInt();
             }
         }
+        System.out.println("Output ");
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println("");
         }
-        searchKey(matrix, 5);
+        System.out.println("Enter the key to search:");
+        int key = sc.nextInt();
+        searchKey(matrix,key);
     }
 }
