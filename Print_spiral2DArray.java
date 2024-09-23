574e4758b201cf709fa1ad2a54b0a74294a2bb8d
Shrawan2002 <shrawankumar1754@gmail.com>
2024-09-23 15:13:57 +0530

    initial files

--- Print_spiral2DArray.java
+++ Print_spiral2DArray.java
@@ -8,32 +8,38 @@ public class Print_spiral2DArray {
         while (startRow <= endRow && startCol <= endCol) {
             //top
             for(int j = startCol; j<=endCol; j++){
-                System.out.print(matrix[startRow][j]);
+                System.out.print(matrix[startRow][j]+ " ");
             }
             //right 
             for(int i = startRow+1; i<=endRow; i++){
-                System.out.print(matrix[i][endCol]);
+                System.out.print(matrix[i][endCol]+ " ");
             }
             //bottom 
-            for(int j = endCol-1; j<=startCol; j--){
+            for(int j = endCol-1; j>=startCol; j--){
                 if (startRow == endRow) {
                     break;
                 }
-                System.out.print(matrix[endRow][j]);
+                System.out.print(matrix[endRow][j] + " ");
             }
             //left
-            for(int i = endRow-1; i<=startRow+1; i--){
+            for(int i = endRow-1; i>=startRow+1; i--){
                 if (startCol == endCol) {
                     break;
                 }
-                System.out.print(matrix[i][startCol]);
+                System.out.print(matrix[i][startCol] + " ");
             }
+            startRow++;
+            endRow--;
+            startCol++;
+            endCol--;
         }
+        System.out.println();
     }
     public static void main(String[] args) {
-        int matrix[][] = { {1, 2, 3, 4,},    
+        int matrix[][] = { {1, 2, 3, 4},    
                            {5, 6, 7, 8},
                            {9, 10, 11, 12},
-                           {13, 14, 15, 16}};     
+                           {13, 14, 15, 16}};  
+                           printSpiral(matrix);   
     }
 }
