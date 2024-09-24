4ba9fa40e9bfe53b43cd4038dd7fb999ec42b2de
Shrawan2002 <shrawankumar1754@gmail.com>
2024-09-24 14:49:51 +0530

    initial files

--- DiagonalSum.java
+++ DiagonalSum.java
@@ -2,13 +2,22 @@ public class DiagonalSum {
 
     public static int diagonalSum(int matrix[][]){
         int sum = 0;
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if (i == j) {
-                    sum += matrix[i][j];
-                }else if (i+j == matrix.length-1) {
-                    sum += matrix[i][j];
-                }
+        //brute force Approach
+        // for(int i=0; i<matrix.length; i++){
+        //     for(int j=0; j<matrix[0].length; j++){
+        //         if (i == j) {
+        //             sum += matrix[i][j];
+        //         }else if (i+j == matrix.length-1) {
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+        for(int i = 0; i<matrix.length; i++){
+            //primary diagonal
+            sum += matrix[i][i];
+            //secondary diagonal
+            if (i != matrix.length-1-i) {
+                sum += matrix[i][matrix.length-1-i];
             }
         }
         return sum;
