1e980b689b095f0cd7fc4efccd14c02d6c95d4b9
Shrawan2002 <shrawankumar1754@gmail.com>
2024-09-30 21:03:01 +0530

    initial files

---
+++ Array_question/FindSecondLargestNumber.java
@@ -0,0 +1,14 @@
+package Array_question;
+
+import java.util.Scanner;
+
+public class FindSecondLargestNumber {
+    public static int[] takeInput(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+    }
+    public static void main(String[] args) {
+        int arr[] =
+    }
+}
